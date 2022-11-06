45df7be4b90a76e7fa0277dcc288aa3010bf900a
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2022-11-06 02:14:48 +0200

    Fix saveResources in presistor

--- persistor/index.js
+++ persistor/index.js
@@ -7,7 +7,7 @@ const STREAM_ID = "0xbf259938539458f01d224d09c29d30babc701709/resourced";
 let resources = [];
 
 // Read resources from json file
-const resourceFile = "resources.json";
+const resourceFile = "./resources.json";
 readFile(resourceFile, (err, data) => {
   resources = JSON.parse(data);
 });
@@ -19,20 +19,20 @@ const streamrClient = new StreamrClient({
 });
 
 function addResourceIfNotExists(newResource) {
-  if (!resources.find((resource) => resource.address === newResource.address)) {
+  if (!resources.find(resource => resource.address === newResource.address)) {
     resources.push(newResource);
   }
 }
 
 function saveResources() {
-  writeFile(resourceFile, JSON.parse(resources), (err) => {
-    throw err;
+  writeFile(resourceFile, JSON.stringify(resources), (err) => {
+    console.error(err);
   });
 }
 
 // Subscribe to a stream
-const subscription = await streamrClient.subscribe(
-  { stream: STREAM_ID },
+streamrClient.subscribe(
+  STREAM_ID,
   (message) => {
     // This function will be called when new messages occur
     message.data.forEach((publishedResource) => {
